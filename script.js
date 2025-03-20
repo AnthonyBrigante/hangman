be42dbf2ad02b56ada5807f1d16d6309a4ccace4
AnthonyBrigante <526abrigante@frhsd.com>
2025-03-20 11:46:14 -0400

    a lot more done

--- script.js
+++ script.js
@@ -16,9 +16,9 @@ const wordlist = [
 
 //declare varibale
 let selectedWord =''
-let displaydWord =''
+let displayWord =''
 let wrongGuesses = 0
-let  guessedLetters = []
+let  guessedLetter = []
 const maxMistakes = 6
 
 
@@ -26,10 +26,13 @@ const maxMistakes = 6
 function startGame(level) {
     //reset game
     wrongGuesses = 0
-    guessedLetters = []
+    guessedLetter = []
 
     selectedWord = getRandomWord(level)
+    displayWord = '_'.repeat(selectedWord.length)
+
     updateDifficultyDisplay(level)
+    updateUI()
     
 
 //show game area and difficulty display, hide selection buttons
@@ -40,6 +43,8 @@ document.getElementById('difficultyBox').classList.remove('d-none')
 document.getElementById('difficultyBox').classList.add('d-block')
 
 document.getElementById('difficultySelection').classList.add('d-none')
+
+document.getElementById('letterInput').focus()
 }
 
 
@@ -70,5 +75,50 @@ difficultyBox.classList.add('easy')
     difficultyBox.innerText = 'difficulty: hard'
     difficultyBox.classList.add('hard')
 }
+
+}
+
+function updateUI() {
+    document.getElementById('wordDisplay').textContent = displayWord.split('').join('  ')
+
+}
+
+function guessedLetters() {
+let inputField = DocumentTimeline.getElementById('letterInput')
+let guessedLetters = inputField.value.toLowerCase()
+
+//check for valid input
+if (!guessedLetters.match(/^[a-z]$/)) {
+    alert('Please enter a valid lowercase letter')
+    inputField.value = ''
+    return
+}
+
+
+
+//check if letter is already guessed
+if(guessedLetters.includes(guessedLetter)){
+    alert(`You already guessed '${guessedLetter}'. try a different letter`)
+}
+
+
+
+
+
+guessedLetters.push(guessedLetter) 
+
+
+
+if(selectedWord.includes(guessedLetter)){
+    updateCorrectGuess(guessedLetter)
+} else{
+    updateWrongGuess(guessedLetter)
+}
+
+inputField.value = ''
+document.getElementById('letterInput').focus()
+
+
+
 
 }
